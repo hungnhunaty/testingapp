014890a5c7d83a1986d2478003bbeb0bac50caa0
Anh Khoa Nguyễn <nguyenanhkhoa2135@gmail.com>
2026-01-30 22:10:15 +0700

    Angular Project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+
+
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
@@ -9,4 +12,33 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('AngularProject');
+  message = "Hello World!";
+  count = 0;
+
+  users = ['Khoa', 'Uyen', 'Hoang', 'Long']
+
+  handleEvent(event: Event){
+    console.log(event.type);
+  }
+
+  answer1: string = 'Có';
+  answer2: string = 'Không';
+
+  setColor: boolean = true;
+  handleYesAnswer(event: Event){
+    if(event.type == "mouseenter"){
+      this.answer2 = "Có";
+      this.answer1 = "Không";
+      this.setColor = true;
+    }
+    else if(event.type == "mouseleave"){
+      this.answer2 = "Không";
+      this.answer1 = "Có";
+      this.setColor = false;
+    }
+  }
+
+  showAlert(){
+    alert("Cảm ơn bé đã trả lời thiệt lòng hí hí hí 🎉🎉🎉 😎😎😎");
+  }
 }
